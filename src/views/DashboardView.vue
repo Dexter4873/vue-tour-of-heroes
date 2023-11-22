7dd7e27ab54d6fce301ee9ce7efeab8515cceff3
Daniel Carrillo <dexter4873@gmail.com>
2023-11-22 15:10:23 -0600

    Add dashboard styles

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,16 +1,39 @@
 <script setup>
-  import { useHeroesStore } from "@/stores/heroes";
+import { useHeroesStore } from '@/stores/heroes'
 
-  const heroesStore = useHeroesStore();
+const heroesStore = useHeroesStore()
 </script>
 
 <template>
-  <h1>Dashboard</h1>
+  <h1>Top Heroes</h1>
   <ul>
     <li v-for="hero in heroesStore.top" :key="hero.id">
-      <router-link :to="`/heroes/detail/${hero.id}`">{{hero.name}}</router-link>
+      <router-link :to="`/heroes/detail/${hero.id}`">{{ hero.name }}</router-link>
     </li>
   </ul>
 </template>
 
-<style scoped></style>
+<style scoped>
+h1 {
+  text-align: center;
+}
+
+ul {
+  display: flex;
+  justify-content: center;
+}
+
+ul li {
+  list-style: none;
+}
+
+ul li a {
+  display: block;
+  margin: 0.5rem;
+  padding: 2rem 2rem;
+  background-color: #288bef;
+  border-radius: 10px;
+  text-decoration: none;
+  color: white;
+}
+</style>
