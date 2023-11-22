52dd4a0e6e870eff318185d30af56afe87508dd4
Daniel Carrillo <dexter4873@gmail.com>
2023-11-22 14:58:50 -0600

    Style app layout

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,41 @@
-<script setup></script>
+<script setup>
+</script>
 
 <template>
   <h1>Tour of heroes</h1>
-  <router-link to="/">Dashboard</router-link>
-  <router-link to="/heroes">Heroes</router-link>
+  <nav>
+    <router-link to="/" exact-active-class="active">Dashboard</router-link>
+    <router-link to="/heroes" exact-active-class="active">Heroes</router-link>
+  </nav>
   <router-view />
 </template>
 
 <style scoped>
+h1 {
+  padding: 1rem;
+  color: #8d8d8d;
+}
+
+nav {
+  margin: 1.5rem 0;
+  padding: 0 1rem;
+}
+
+nav a {
+  text-decoration: none;
+  margin-right: .8rem;
+  padding: 0.8rem;
+  color: #3f6573;
+  background-color: rgba(180, 179, 179, 0.76);
+  border-radius: 5px;
+  transition: background-color 0.2s;
+}
+
+nav a:hover {
+  background-color: rgba(180, 179, 179, 0.93);
+}
+
+.active {
+  color: #82b8f1
+}
 </style>
